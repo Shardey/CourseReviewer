06ff92fee3ec6964410b9fb6f3fa1cf44fe5a688
Tomi Laakso <tomi.m.laakso@gmail.com>
2016-11-08 15:47:41 +0200

    Loginin aloitus

--- ratings/urls.py
+++ ratings/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # /ratings/CS-C4000/
     url(r'^(?P<course_code>[0-9a-zA-Z\-]*)/$', views.results, name='results'),
+    # Registration URLs
+    url(r'^accounts/register/$', 'simplesite.views.register', name='register'),
+    url(r'^accounts/register/complete/$', 'simplesite.views.registration_complete', name='registration_complete'),
 ]
+
+
--- ratings/views.py
+++ ratings/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.core.context_processors import csrf
 
 def index(request):
     return HttpResponse("Hello, you are at ratings index.")
@@ -7,5 +11,21 @@ def results(request, course_code):
     response = "You have searched for the course %s"
     return HttpResponse(response % course_code)
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/accounts/register/complete')
 
+    else:
+        form = UserCreationForm()
+    token = {}
+    token.update(csrf(request))
+    token['form'] = form
+
+    return render_to_response('registration/registration_form.html', token)
+
+def registration_complete(request):
+    return render_to_response('registration/registration_complete.html')
 
