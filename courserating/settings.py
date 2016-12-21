0fa48ef245bf9f5df4f305022e5d8bef6a0ea1a6
Timo Rutanen <trutanen@sitriini.org.aalto.fi>
2016-12-21 16:36:47 +0200

    typo in settings file corrected

--- courserating/settings.py
+++ courserating/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Heroku stuff begin
 
-import dj_database_url.config(conn_max_age=500)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
