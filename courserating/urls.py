e2205b5acb4b88fc7bcf49b3932ecbc9d7a8f7af
Timo Rutanen <trutanen@sitriini.org.aalto.fi>
2016-12-21 17:55:19 +0200

    Trying to move from / to /ratings automatically in heroku

--- courserating/urls.py
+++ courserating/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url[r'', include('ratings.urls'),
     url(r'^admin/', admin.site.urls),
     url(r'^ratings/', include('ratings.urls')),
 ]
